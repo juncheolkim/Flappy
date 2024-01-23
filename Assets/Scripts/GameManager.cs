0c736aeb522b46b7d0d54d5f371a67a630bfdece
juncheolkim <kimjoon889@gmail.com>
2024-01-23 23:05:08 +0900

    충돌에 따른 점수 계산 및 게임 오버 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
     private int score;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOver.SetActive(false);
+        playButton.SetActive(false);
+
+        Time.timeScale = 1;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            // Pipes 자체를 삭제하는 것이 아니라 게임오브젝트를 Destroy해야한다.
+            Destroy(pipes[i].gameObject);
+        }
+    }
+    public void Pause()
+    {
+        // 플레이어의 모든 움직임은 deltaTime을 곱해서 움직인다.
+        // 아래 설정을 통해서 플레이어가 멈추게 된다.
+        Time.timeScale = 0;
+        player.enabled = false;
+    }
+
     public void GameOver()
     {
-        Debug.Log("Game over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+        Pause();
     }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 
 }
